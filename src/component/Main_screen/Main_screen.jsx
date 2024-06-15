1e1f35bc78ece729be9e238f1bb29ec8fcf392e4
Ilyes Soumar Djouma <ilyes.soumar@free.fr>
2024-06-16 01:30:38 +0200

    first screen component

--- src/component/Main_screen/Main_screen.jsx
+++ src/component/Main_screen/Main_screen.jsx
@@ -1,16 +1,20 @@
 import Header from "../../layer/Header/Header";
 import Footer from "../../layer/Footer/Footer";
-import Button_command from "../objet/Button_command/Button_command";
 import Pannel_commands from "../Panel_commands/Pannel_commands";
 
 function Main_screen() {
+
+
+
   return (
     <>
       <Header />
       <div className="main_screen-container">
         <div className="wrapper-left"></div>
         <div className="wrapper-center">
-          <div className="screen"></div>
+          <div className="screen-container">
+            <div className="screen"></div>
+          </div>
           <Pannel_commands />
         </div>
         <div className="wrapper-right"></div>
--- src/component/Panel_commands/Pannel_commands.jsx
+++ src/component/Panel_commands/Pannel_commands.jsx
@@ -1,27 +1,36 @@
 import Button_command from "../objet/Button_command/Button_command";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import { useState } from "react";
 
-function Pannel_commands({actionFunc}) {
+function Pannel_commands() {
+  const [counter, setCounter] = useState();
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+    console.log(counter)
+  };
+
+
   return (
     <div className="button-commands">
       <div className="button-commands-wrapper-left">
-        <Button_command cmd="a" name="A"actionFunc={actionFunc} />
-        <Button_command cmd="left" name="Left"actionFunc={actionFunc} />
+        <Button_command cmd="a" name="A" actionFunc={handleClick} />
+        <Button_command cmd="left" name="Left" actionFunc={handleClick} />
       </div>
       <div className="button-commands-wrapper-central">
-        <Button_command cmd="up" name="Up" actionFunc={actionFunc}/>
-        <Button_command cmd="down" name="Down" actionFunc={actionFunc}/>
+        <Button_command cmd="up" name="Up" actionFunc={handleClick} />
+        <Button_command cmd="down" name="Down" actionFunc={handleClick} />
       </div>
       <div className="button-commands-wrapper-right">
-        <Button_command cmd="right" name="Right" actionFunc={actionFunc}/>
-        <Button_command cmd="b" name="B" actionFunc={actionFunc} />
+        <Button_command cmd="right" name="Right" actionFunc={handleClick} />
+        <Button_command cmd="b" name="B" actionFunc={handleClick} />
       </div>
     </div>
   );
 }
 
 Pannel_commands.propTypes = {
-  actionFunc:PropTypes.func.isRequired,
- }
+  actionFunc: PropTypes.func.isRequired,
+};
 
 export default Pannel_commands;
